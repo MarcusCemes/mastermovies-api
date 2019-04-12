964d6794ac5ed0a7025a22caf21b7efb03c0b221
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2019-04-12 18:34:02 +0200

    Base app
    Basic router integration and root ('/') GET response

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+// Entry point, spin up the ExpressJS server
 import express from "express";
+import routes from "./routes";
 const app = express();
 
-const PORT = 3000;
-app.listen(PORT, () => process.stdout.write("MasterMovies API is listening on port " + PORT));
+const PORT = process.env.PORT || 3000;
+app.use(routes);
+app.listen(PORT, () =>
+  process.stdout.write("MasterMovies API is listening on http://127.0.0.1:" + PORT + "\n"
+));
